5645b62581fa3c8543ff77b52e71f7a04e567ba8
“Akanni23“ <muhasy2k@yahoo.com>
2024-08-03 04:04:48 -0700

    corrected

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,24 +1,11 @@
 #!/usr/bin/node
-import sys
 
-def read_and_print_file(file_path):
-    """
-    Reads and prints the content of a file.
+const fs = require('fs');
 
-    Parameters:
-    file_path (str): The path to the file to read.
-    """
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <file_path>")
-    else:
-        file_path = sys.argv[1]
-        read_and_print_file(file_path)
-
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
